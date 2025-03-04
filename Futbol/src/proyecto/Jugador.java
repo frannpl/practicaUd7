368d10c9a5c4d06dfa16928ebead92cd1e20103e
Franflii <franlopezpineda@gmail.com>
2025-03-04 15:58:32 +0100

    asdfas

--- Futbol/src/proyecto/Jugador.java
+++ Futbol/src/proyecto/Jugador.java
@@ -2,10 +2,16 @@ package proyecto;
 
 public class Jugador {
 // Atributos
-private String nombre;
-private int dorsal;
-private String equipo;
+	private String nombre;
+	private int dorsal;
+	private String equipo;
 
+//Constructor
 
+	public Jugador(String nombre, int dorsal, String equipo) {
 
+		this.nombre = nombre;
+		this.dorsal = dorsal;
+		this.equipo = equipo;
+	}
 }
