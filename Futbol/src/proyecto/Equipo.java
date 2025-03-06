180e3c5bbba94970987cea41af04eff5cafb21fc
huertas13 <d.h.montero47@hotmail.com>
2025-03-06 12:22:58 +0100

    fdafda

---
+++ Futbol/src/proyecto/Equipo.java
@@ -0,0 +1,104 @@
+package proyecto;
+
+public class Equipo {
+
+// atributos
+	
+	String [] jugadores;;
+
+
+
+
+
+// Constructor 
+	
+	public Equipo() {
+		
+		this.jugadores = jugadores;
+		
+	}
+
+		
+	
+		//getters y setters 
+	
+	public String[] getJugadores() {
+		return jugadores;
+	}
+
+
+
+	public void setJugadores(String[] jugadores) {
+		this.jugadores = jugadores;
+	}
+
+	
+	
+	
+	
+	
+	// Metodos 
+	
+	
+	public void addJugador(Jugador jugador) {
+		
+		
+		if (jugadores.equals(null)) {
+			
+			throw new IllegalArgumentException("El jugador no puede ser nulo");
+			
+		}
+		
+		else {
+			for (int i = 0; i < jugadores.length -1; i++) {
+				
+				jugadores[i] = 
+				
+				
+			}
+			
+			
+		}
+		
+	
+	
+	
+	
+	
+	}
+	
+		
+
+
+		public void mostrarJugador() {
+			
+		
+
+			for (int i = 0; i < jugadores.length -1; i++) {
+				
+				
+				
+				
+				
+				
+				
+				
+				
+			}
+			
+		
+		
+		
+		
+		
+		}
+	
+	
+	
+}
+	
+	
+
+
+
+
